64180d689cf0da56f5125720ac1f69d96b7800ef
Jordan <jordanjlatimer@gmail.com>
2020-09-21 10:14:34 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { IoIosBusiness, IoIosCart, IoIosClipboard, IoIosCube, IoIosPeople, IoIosSpeedometer } from 'react-icons/io';
+import Content from './Content';
+import Sidebar from './Sidebar/Sidebar'
+import SidebarAvatar from './Sidebar/SidebarAvatar';
+import SidebarItem from './Sidebar/SidebarItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  const [curMod, setCurMod] = useState("customers")
+
+  return(
+    <>
+      <Sidebar>
+        <SidebarAvatar username="Username"/>
+        <SidebarItem active={curMod === "dashboard"} icon={<IoIosSpeedometer size="20px"/>} label="Dashboard" onClick={() => setCurMod("dashboard")}/>
+        <SidebarItem active={curMod === "customers"} icon={<IoIosPeople size="20px"/>} label="Customers" onClick={() => setCurMod("customers")}/>
+        <SidebarItem active={curMod === "treatments"} icon={<IoIosClipboard size="20px"/>} label="Treatments" onClick={() => setCurMod("treatments")}/>
+        <SidebarItem active={curMod === "orders"} icon={<IoIosCart size="20px"/>} label="Orders" onClick={() => setCurMod("orders")}/>
+        <SidebarItem active={curMod === "items"} icon={<IoIosCube size="20px"/>} label="Items" onClick={() => setCurMod("items")}/>
+        <SidebarItem active={curMod === "vendors"} icon={<IoIosBusiness size="20px"/>} label="Vendors" onClick={() => setCurMod("vendors")}/>
+      </Sidebar>
+      <Content module={curMod}/>
+    </>
+  )
 }
-
-export default App;
